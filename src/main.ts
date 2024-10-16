19e1c484a3fdd47356373f29ef681261c9f6c84d
HengyangYe <heye@ucsc.edu>
2024-10-16 01:41:15 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,22 @@ app.append(header);
 
 const button = document.createElement("button");
 button.innerText = "❤️ A Normal Button ❤️";
-button.style.padding = "20px 40px";
 button.style.fontSize = "1.5rem";
 button.style.fontWeight = "bold";
-button.style.borderWidth = "5px";
+button.style.padding = "20px 40px";
+
+let counter: number = 0;
+
+const counterDiv = document.createElement("div");
+counterDiv.innerText = `${counter} ❤️`;
+counterDiv.style.fontSize = "1.5rem";
+counterDiv.style.marginTop = "20px";
+
+
+button.addEventListener("click", () => {
+  counter++;
+  counterDiv.innerText = `${counter} ❤️`;
+});
 
 button.addEventListener("mousedown", () => {
   button.style.transform = "scale(0.95)";
@@ -29,4 +41,6 @@ button.addEventListener("mouseup", () => {
 button.addEventListener("mouseleave", () => {
   button.style.transform = "scale(1)";
 });
+
 app.appendChild(button);
+app.appendChild(counterDiv);
