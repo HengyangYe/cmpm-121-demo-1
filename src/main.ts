e4c6062e06e2b54d310797b96f74e4cf20523846
HengyangYe <heye@ucsc.edu>
2024-10-16 01:26:12 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My Lovely game";
+const gameName = "My L❤️vely game";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerText = "❤️ A Normal Button ❤️";
+button.style.padding = "20px 40px";
+button.style.fontSize = "1.5rem";
+button.style.fontWeight = "bold";
+button.style.borderWidth = "5px";
+app.appendChild(button);
